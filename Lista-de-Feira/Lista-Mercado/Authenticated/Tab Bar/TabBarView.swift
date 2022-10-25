835b4f204fc919ee2696401162cb5ad14f39edcc
Marbarret <marsouza93@gmail.com>
2022-10-25 11:18:59 -0300

    Tab bar app

---
+++ Lista-de-Feira/Lista-Mercado/Authenticated/Tab Bar/TabBarView.swift
@@ -0,0 +1,42 @@
+//
+//  TabBarView.swift
+//  Lista-Mercado
+//
+//  Created by Marcylene Barreto on 23/10/22.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    @State var current = "Home"
+    
+    var body: some View {
+        ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+            
+            TabView(selection: $current) {
+                Text("Home")
+                    .tag("Home")
+                
+                Text("Carteira")
+                    .tag("Carteira")
+                
+                Text("Perfil")
+                    .tag("Perfil")
+            }
+            
+            HStack(spacing: 0) {
+                TabButton(title: "Home", image: "house", selected: $current)
+                
+                TabButton(title: "Carteira", image: "wallet.pass", selected: $current)
+                
+                TabButton(title: "Perfil", image: "person.fill", selected: $current)
+            }
+            .padding(.vertical, 12)
+            .padding(.horizontal)
+            .background(Color.theme.bluePrimary)
+            .clipShape(Capsule())
+            .padding(.horizontal, 25)
+        }
+    }
+}
+
